ab0736ea04a5f066ba103aeb83d9cad958fe0005
david <miharisoadavidfils@gmail.com>
2025-07-17 17:10:39 +0300

    feat: add Snake class, direction control and movement logic

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,14 +1,63 @@
 import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.awt.Color;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.LinkedList;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
     public static final int WIDTH = 600;
     public static final int HEIGHT = 600;
+    private static final int TILE_SIZE = 20;
+    private static final int DELAY = 100;
+
+    private Timer timer;
+    private Snake snake;
 
     public GamePanel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
-        setBackground(Color.BLACK); // Just a black background for now
+        setBackground(Color.BLACK);
         setFocusable(true);
+        addKeyListener(this);
+
+        snake = new Snake(WIDTH / TILE_SIZE / 2, HEIGHT / TILE_SIZE / 2);
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        snake.move();
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        drawSnake(g);
+    }
+
+    private void drawSnake(Graphics g) {
+        g.setColor(Color.GREEN);
+        for (SnakeSegment segment : snake.getBody()) {
+            g.fillRect(segment.x * TILE_SIZE, segment.y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP -> snake.setDirection(Direction.UP);
+            case KeyEvent.VK_DOWN -> snake.setDirection(Direction.DOWN);
+            case KeyEvent.VK_LEFT -> snake.setDirection(Direction.LEFT);
+            case KeyEvent.VK_RIGHT -> snake.setDirection(Direction.RIGHT);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {}
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
 }
